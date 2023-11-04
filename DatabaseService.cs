27b1e68eeb39da6f920f6d8f290602949ff814b0
Jakub Włodarczyk <jakub.wlodarczyk@student.po.edu.pl>
2023-11-04 17:37:02 +0100

    IDatabaseService implementation created

---
+++ DatabaseService.cs
@@ -0,0 +1,8 @@
+namespace Git.Laboratory;
+
+public class DatabaseService : IDatabaseService
+{
+    public bool CustomerExists(string code){
+        // make database call to recieve information wheather customer exists 
+    }
+}
